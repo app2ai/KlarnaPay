fd5a3a3a1188bc8541df9e6ecc2906a9cce38558
Vishal Rana <job.vishalrana@gmail.com>
2026-06-21 21:28:42 +0530

    update appcompat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation(libs.androidx.appcompat)
 
     // Compose
     implementation(platform(libs.androidx.compose.bom))
